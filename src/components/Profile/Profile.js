1bb57e3d417dbb8f81f885c030ae6f11bfb2d90b
Roman Pryymak <prima3110@ukr.net>
2019-12-09 10:10:22 +0200

    corrected hw-01

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -2,27 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Profile.module.css';
 
-const Profile = ({ user }) => (
+const Profile = ({ user: { avatar, name, tag, location, stats } }) => (
   <div className={styles.profile}>
     <div className={styles.description}>
-      <img src={user.avatar} alt="user avatar" className={styles.avatar} />
-      <p className={styles.name}>{user.name}</p>
-      <p className={styles.tag}>@{user.tag}</p>
-      <p className={styles.location}>{user.location}</p>
+      <img src={avatar} alt="user avatar" className={styles.avatar} />
+      <p className={styles.name}>{name}</p>
+      <p className={styles.tag}>@{tag}</p>
+      <p className={styles.location}>{location}</p>
     </div>
 
     <ul className={styles.stats}>
       <li className={styles.item}>
         <span className={styles.label}>Followers</span>
-        <span className={styles.quantity}>{user.stats.followers}</span>
+        <span className={styles.quantity}>{stats.followers}</span>
       </li>
       <li className={styles.item}>
         <span className={styles.label}>Views</span>
-        <span className={styles.quantity}>{user.stats.views}</span>
+        <span className={styles.quantity}>{stats.views}</span>
       </li>
       <li className={styles.item}>
         <span className={styles.label}>Likes</span>
-        <span className={styles.quantity}>{user.stats.likes}</span>
+        <span className={styles.quantity}>{stats.likes}</span>
       </li>
     </ul>
   </div>
